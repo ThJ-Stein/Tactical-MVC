53e455989bb9cd97443e5f5030f4bdad1ac42339
Thomas <t.j.stein@student.utwente.nl>
2017-02-06 11:28:49 +0100

    Remove engine from implementation branch

--- src/engine/Model.java
+++
@@ -1,8 +0,0 @@
-package engine;
-
-/**
- * Created by thomas on 4-2-17.
- */
-public abstract class Model {
-
-}
--- src/engine/command/InputCommandHandler.java
+++
@@ -1,8 +0,0 @@
-package engine.command;
-
-/**
- * Created by thomas on 4-2-17.
- */
-public interface InputCommandHandler {
-    void executeCommand(InputCommand command);
-}
